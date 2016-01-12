764c6b5270d4a4e238f162d3c7ad3ef2e3045698
Vijay <vshukla684@gmail.com>
2016-01-12 16:20:37 +0530

    Implement First way of Custom Rendering

--- grails-app/conf/spring/resources.groovy
+++ grails-app/conf/spring/resources.groovy
@@ -1,3 +1,9 @@
+import grails.rest.render.json.JsonCollectionRenderer
+import restapi.Country
+
 // Place your Spring DSL code here
 beans = {
+    countryJsonRenderer(JsonCollectionRenderer, Country) {
+        excludes = ['abreviation']
+    }
 }
