2b5d9fd0698e9b4e21ce7e1ed9c3fd1701fb557f
Vijay <vshukla684@gmail.com>
2016-01-12 16:23:29 +0530

    Implement First way of Custom XML Rendering

--- grails-app/conf/spring/resources.groovy
+++ grails-app/conf/spring/resources.groovy
@@ -1,9 +1,15 @@
+import com.Person
 import grails.rest.render.json.JsonCollectionRenderer
+import grails.rest.render.xml.XmlCollectionRenderer
 import restapi.Country
 
 // Place your Spring DSL code here
 beans = {
     countryJsonRenderer(JsonCollectionRenderer, Country) {
         excludes = ['abreviation']
+    }
+
+    personJsonRenderer(XmlCollectionRenderer, Person){
+        excludes = ["age"]
     }
 }
